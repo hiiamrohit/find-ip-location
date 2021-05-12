460c35e63bd10a5eab8604239652b5e99c80ac63
Rohit Kumar <rohit0kumar@hotmail.com>
2021-05-12 16:37:58 +0530

    Update index.php
    client ip issue fixed

--- index.php
+++ index.php
@@ -19,13 +19,31 @@ function ipInfo($ip) {
 		echo "IP is not set!"; 
 	}
 }
-
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
 if(isset($_REQUEST['ip'])) {
 	$data = ipInfo($_REQUEST['ip']);
   $uIp = $_REQUEST['ip'];
 } else {
-	$data = ipInfo($_SERVER['REMOTE_ADDR']);
-   $uIp = $_SERVER['REMOTE_ADDR'];
+	$uIp =  get_client_ip();
+	$data = ipInfo($uIp);
+   
 }
 
 ?>
